f202eff2b23aec1f75205ac212fb2bd1c4ad4e17
jsnelgro <johnny.snelgrove@gmail.com>
2016-05-24 22:36:26 -0400

    add smooth scrolling

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", function(event) {
+  smoothScroll.init({speed: 650})
   moment.tz.add('America/Los_Angeles|PST PDT|80 70|0101|1Lzm0 1zb0 Op0');
   var closingTimeSpan = document.getElementById('js-timeUntilClosing')
   var callBtn = document.getElementById('js-call-btn')
