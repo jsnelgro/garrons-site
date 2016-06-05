c4f099ca030e5fc8b7ad527c7a36500b50e1e7b2
jsnelgro <johnny.snelgrove@gmail.com>
2016-06-05 17:05:26 -0400

    Update 2016-06-05T21:05:25.763Z

--- js/main.js
+++ js/main.js
@@ -1,22 +1,28 @@
 document.addEventListener("DOMContentLoaded", function(event) {
   smoothScroll.init({speed: 650})
-  moment.tz.add('America/Los_Angeles|PST PDT|80 70|0101|1Lzm0 1zb0 Op0');
+  moment.tz.add('America/Los_Angeles|PST PDT|80 70|0101|1Lzm0 1zb0 Op0')
+  document.getElementById('currYearCopyright').innerText = moment(new Date()).tz('America/Los_Angeles').year()
   var closingTimeSpan = document.getElementById('js-timeUntilClosing')
   var callBtn = document.getElementById('js-call-btn')
   var schedule = [
     [0, 0],   // sun
-    [10, 17], // mon
-    [10, 17], // tues
-    [10, 17], // wed
-    [10, 17], // thurs
-    [10, 17], // fri
+    [9, 17], // mon
+    [9, 17], // tues
+    [9, 17], // wed
+    [9, 17], // thurs
+    [9, 17], // fri
     [0, 0]    // sat
   ]
   var getTimes = function() {
     var currentDate = moment(new Date()).tz('America/Los_Angeles')
     var untilClose = currentDate.to(currentDate.clone().set('hour', schedule[currentDate.day()][1]))
     if (untilClose.includes('ago')) {
-      var untilOpen = currentDate.to(currentDate.clone().set('hour', schedule[currentDate.day() + 1][0]).set('day', currentDate.day() + 1))
+      var tomorrow = (currentDate.day() + 1) % schedule.length
+      var untilOpen = currentDate.to(
+        currentDate.clone()
+        .set('hour', schedule[tomorrow][0])
+        .set('day', currentDate.day() + 1)
+      )
       closingTimeSpan.innerText = `opens ${untilOpen}`
     }
     else {
@@ -34,7 +40,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
   callBtn.addEventListener('click', function(e) {
     var telA = document.createElement('a')
-    telA.href = `tel:${[6, 1, 7, 3, '-', 1, 0, 3, '-', 0, 6, 3, '-', 1].reverse().join('')}`
+    telA.href = `tel:${[1, 4, 3, 3, '-', 5, 8, 3, '-', 0, 6, 3, '-', 1].reverse().join('')}`
     telA.click()
   })
 })
